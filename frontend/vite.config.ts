205f903b9e469bb2a2c8a3127fa5212465a72781
TobiLight <jgetitdonefast@gmail.com>
2024-05-27 12:18:49 +0100

    simple 2fa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,18 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
+import { vercelPreset } from "@vercel/remix/vite";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
+  plugins: [
+    remix({
+      presets: [vercelPreset()],
+    }),
+    tsconfigPaths(),
+  ],
   build: {
-    outDir: '../build'
-  }
+    outDir: "../build",
+  },
 });
